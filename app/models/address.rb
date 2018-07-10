e5de58134f6caf952bf481f4e1a5a1027a4ce462
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-07-10 00:25:49 +0000

    add validation in Address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,14 @@
 class Address < ApplicationRecord
+  validates :zip_5, length: { is: 5 }, numericality: { only_integer: true }, presence: true
+  validates :street_name, :house_number, :city, presence: true
+  validates :state, length: { is: 2 }, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  
   def to_s
     # TODO: override the to_s method so that it prints out the address components as follows
     # house_number street_predirection street_name street_type street_postdirection unit_type unit_number, city, state, zip_5
   end
 end
+
+
+# - state (saved as 2 letter state abbreviation)
+# - 5 digit zip code (only numbers) 
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+# require 'spec_helper'
 
 RSpec.describe Address, :type => :model do
   describe '#new' do
