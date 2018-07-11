91b3ca4f14e8f980c00924352959a0797c2320fb
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-07-11 15:26:57 +0000

    add styling

--- app/assets/javascripts/addresses.js
+++ app/assets/javascripts/addresses.js
@@ -12,13 +12,13 @@ $(document).ready(function () {
         var state =  $("input[id='state']").val();
         var zip =  $("input[id='zip_code']").val();
         var singleadd = `${st},  ${city}, ${state}, ${zip}`;
-        var token = "YwnlcGlz5qmtcE0PTC6T82YfXPwac_kBUiqlwuceTqCW48QxxOuR1JtIfb4LjSit3wznWl7PMokTYw78HDIHzAUuy5z7tPhr_lDOIvosPTdxLTg2hhiuVANFRKvSd17LHfo2I2jzEaxcY8wvtj-1gQ.."
+        var token = "nn14gp-ix5OTDbhf4W5hox9GQq-z_dutggVIhgwOTiZ_ICffJ5n6qW5Nz17Oi-cxN5nL5j7e3oZmJeuaQbNjaHt5HlyopZpPGkMPHk9Xhcv3lP7TF8oa4rzPcO0IEtkjRwMAD86ejLN-m-Qq74vcnA.."
         var url = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?singleLine=" + singleadd +"&maxLocations=1&forStorage=true&outFields=AddNum,StName,StType,StPreDir,StDir,UnitType,UnitName,City,RegionAbbr,Postal&token=" + token +"&f=pjson"
 
         fetch(url)
           .then((resp) => resp.json())
           .then(function(data) {
-            $.post("http://ec2-18-191-248-3.us-east-2.compute.amazonaws.com:8080/addresses", { address: data["candidates"][0] })
+            $.post("http://ec2-18-219-94-25.us-east-2.compute.amazonaws.com:8080/addresses", { address: data["candidates"][0] })
               
           })
       
